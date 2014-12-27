3e161649e9aa700914ba542fda10ef1f3174c7a6
Amit Kapadia <akapad@gmail.com>
2014-12-27 08:42:36 -0800

    helper functions to parse xml response

---
+++ usgs/xsi.py
@@ -0,0 +1,50 @@
+
+
+def _get_xsd_boolean(element):
+    return True if element.text.lower() == "true" else False
+
+
+def _get_xsd_int(element):
+    return int(element.text)
+
+
+def _get_xsd_double(element):
+    return float(element.text)
+
+
+def _get_xsd_string(element):
+    return element.text
+
+
+def _get_xsd_array(element):
+    items = element.findall("item")
+    
+    if len(items) > 0:
+        data = map(lambda item: { el.tag: get(el) for el in item }, items)
+    else:
+        data = { el.tag: get(el) for el in element }
+    return data
+
+
+def _get_none(element):
+    return None
+
+
+_dtypes = {
+    "xsd:string": _get_xsd_string,
+    "xsd:boolean": _get_xsd_boolean,
+    "xsd:int": _get_xsd_int,
+    "xsd:double": _get_xsd_double,
+    
+    "ns1:ArrayOfService_Inventory_DisplayListValue": _get_xsd_array,
+    "ns1:Service_Inventory_Bounds": _get_xsd_array,
+    "ns1:Service_Class_Coordinate": _get_xsd_array,
+    
+    None: _get_none
+}
+
+
+def get(element):
+    key = element.attrib.get("{http://www.w3.org/2001/XMLSchema-instance}type")
+    return _dtypes[key](element)
+
