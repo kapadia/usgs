49aabf070ea8461f37e472e6173a9c4f09b4f63a
Amit Kapadia <akapad@gmail.com>
2015-01-05 12:45:05 -0800

    cli search

--- usgs/scripts/cli.py
+++ usgs/scripts/cli.py
@@ -1,22 +1,10 @@
 
 import os, json
 import click
-import usgs
+from usgs import api
 
 
-@click.group()
-def usgs():
-    pass
-
-
-@click.command()
-@click.argument("dataset")
-@click.argument("scene-ids", nargs=-1)
-@click.option("--node")
-@click.option("--api-key")
-def metadata(dataset, scene_ids, node, api_key=None):
-    
-    from usgs import api
+def get_node(dataset, node):
     
     if node is None:
         
@@ -29,41 +17,43 @@ def metadata(dataset, scene_ids, node, api_key=None):
         
         node = datasets[dataset].upper()
     
+    return node
+
+
+@click.group()
+def usgs():
+    pass
+
+
+@click.command()
+@click.argument("dataset")
+@click.argument("scene-ids", nargs=-1)
+@click.option("--node", help="The node corresponding to the dataset (CWIC, EE, HDDS, LPVS).", default=None)
+@click.option("--api-key", help="API key returned from USGS servers after logging in.", default=None)
+def metadata(dataset, scene_ids, node, api_key):
+    
+    node = get_node(dataset, node)
+    
     data = api.metadata(dataset, node, scene_ids, api_key)
     print json.dumps(data)
 
 
-# @click.command()
-# @click.option("--start-date")
-# @click.option("--end-date")
-# @click.option("--longitude")
-# @click.option("--latitude")
-# @click.option("--dataset", default="LANDSAT_8")
-# @click.option('--output', help="Output a JSON file containing metadata", default=None)
-# def search(start_date, end_date, longitude, latitude, dataset, output):
-#     import csv, json
-#
-#     from mapbox.usgs.authentication import login, logout
-#     from mapbox.usgs.search import get_scenes
-#
-#     api_key = login()
-#     scenes = get_scenes(
-#         start_date=start_date,
-#         end_date=end_date,
-#         longitude=longitude,
-#         latitude=latitude,
-#         dataset=dataset,
-#         api_key=api_key)
-#
-#     if output:
-#         with open(output, "w") as dst:
-#             dst.write(json.dumps(scenes))
-#
-#     scene_ids = map(lambda d: d["entityId"], scenes)
-#
-#     for scene_id in scene_ids:
-#         print scene_id
+@click.command()
+@click.argument("dataset")
+@click.option("--node", help="The node corresponding to the dataset (CWIC, EE, HDDS, LPVS).", default=None)
+@click.option("--start-date")
+@click.option("--end-date")
+@click.option("--longitude")
+@click.option("--latitude")
+@click.option("--distance", help="Radius - in units of meters - used to search around the specified longitude/latitude.", default=100)
+@click.option("--api-key", help="API key returned from USGS servers after logging in.", default=None)
+def search(dataset, node, start_date, end_date, longitude, latitude, distance, api_key):
+    
+    node = get_node(dataset, node)
+    
+    data = api.search(dataset, node, lat=latitude, lng=longitude, distance=distance, start_date=start_date, end_date=end_date, api_key=api_key)
+    print json.dumps(data)
 
 
-# usgs.add_command(search)
 usgs.add_command(metadata)
+usgs.add_command(search)
