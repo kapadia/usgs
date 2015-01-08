1f4b0df3a5af8977fa7b0778a0eae7c9cda724ef
Amit Kapadia <akapad@gmail.com>
2015-01-08 11:38:15 -0500

    test login xml request

--- tests/test_soap.py
+++ tests/test_soap.py
@@ -1,6 +1,7 @@
 
 import unittest
 from usgs import soap
+from xml.dom import minidom
 
 try:
     from django.test.utils import compare_xml
@@ -13,7 +14,19 @@ class SoapTest(unittest.TestCase):
     
     def test_login(self):
         
-        xml1 = soap.login("username", "password")
-        xml2 = soap.login("username", "password")
+        expected = """
+        <soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soap="http://earthexplorer.usgs.gov/inventory/soap">
+          <soapenv:Header/>
+          <soapenv:Body>
+            <soap:login soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+              <username xsi:type="xsd:string">username</username>
+              <password xsi:type="xsd:string">password</password>
+            </soap:login>
+            </soapenv:Body>
+        </soapenv:Envelope>
+        """
         
-        assert compare_xml(xml1, xml2)
+        request = soap.login("username", "password")
+        request = minidom.parseString(request).toprettyxml()
+        
+        assert compare_xml(request, expected)
