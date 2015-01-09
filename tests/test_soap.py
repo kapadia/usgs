1887f43e7ccd21acbd7442fdef59ac36783ec9f0
Amit Kapadia <akapad@gmail.com>
2015-01-09 14:42:27 -0500

    rearrange tests

--- tests/test_soap.py
+++ tests/test_soap.py
@@ -12,47 +12,6 @@ except ImportError:
 
 @unittest.skipIf(compare_xml is None, "Django's test utilities are required.")
 class SoapTest(unittest.TestCase):
-    
-    
-    def test_login(self):
-        
-        expected = """
-        <soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soap="http://earthexplorer.usgs.gov/inventory/soap">
-          <soapenv:Header/>
-          <soapenv:Body>
-            <soap:login soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-              <username xsi:type="xsd:string">username</username>
-              <password xsi:type="xsd:string">password</password>
-            </soap:login>
-            </soapenv:Body>
-        </soapenv:Envelope>
-        """
-        
-        request = soap.login("username", "password")
-        request = minidom.parseString(request).toprettyxml()
-        
-        assert compare_xml(request, expected)
-    
-    
-    def test_dataset_fields(self):
-        
-        expected = """
-        <soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soap="http://earthexplorer.usgs.gov/inventory/soap">
-          <soapenv:Header/>
-          <soapenv:Body>
-            <soap:datasetFields soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-              <datasetName xsi:type="xsd:string">LANDSAT_8</datasetName>
-              <node xsi:type="xsd:string">EE</node>
-              <apiKey xsi:type="xsd:string">USERS API KEY</apiKey>
-            </soap:datasetFields>
-          </soapenv:Body>
-        </soapenv:Envelope>
-        """
-        
-        request = soap.dataset_fields("LANDSAT_8", "EE", api_key="USERS API KEY")
-        request = minidom.parseString(request).toprettyxml()
-        
-        assert compare_xml(request, expected)
 
 
     def test_clear_bulk_download_order(self):
@@ -142,13 +101,18 @@ class SoapTest(unittest.TestCase):
           <soapenv:Header/>
           <soapenv:Body>
             <soap:datasetFields soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-              <apiKey xsi:type="xsd:string">USERS API KEY</apiKey>
               <datasetName xsi:type="xsd:string">LANDSAT_8</datasetName>
               <node xsi:type="xsd:string">EE</node>
+              <apiKey xsi:type="xsd:string">USERS API KEY</apiKey>
             </soap:datasetFields>
           </soapenv:Body>
         </soapenv:Envelope>
         """
+        
+        request = soap.dataset_fields("LANDSAT_8", "EE", api_key="USERS API KEY")
+        request = minidom.parseString(request).toprettyxml()
+        
+        assert compare_xml(request, expected)
     
        
     def test_download(self):
@@ -259,10 +223,14 @@ class SoapTest(unittest.TestCase):
               <username xsi:type="xsd:string">username</username>
               <password xsi:type="xsd:string">password</password>
             </soap:login>
-          </soapenv:Body>
+            </soapenv:Body>
         </soapenv:Envelope>
         """
-    
+        
+        request = soap.login("username", "password")
+        request = minidom.parseString(request).toprettyxml()
+        
+        assert compare_xml(request, expected)
     
     
     def test_logout(self):
