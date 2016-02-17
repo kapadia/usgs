365bc676977af19dbe366e2bb5efbd83aa4b764e
Scisco <scisco7@gmail.com>
2016-02-17 13:35:58 -0500

    convert  spaces

--- usgs/api.py
+++ usgs/api.py
@@ -1,4 +1,4 @@
-   
+
 import os
 import json
 from xml.etree import ElementTree
@@ -17,25 +17,25 @@ NAMESPACES = {
 }
 
 def _get_api_key():
-    
+
     api_key = None
-    
+
     if os.path.exists(TMPFILE):
         with open(TMPFILE, "r") as f:
             api_key = f.read()
-    
+
     return api_key
 
 
 def _check_for_usgs_error(root):
 
     fault_code_el = root.find("SOAP-ENV:Body/SOAP-ENV:Fault/faultcode", NAMESPACES)
-    
+
     if fault_code_el is None:
         return
-    
+
     fault_string_el = root.find("SOAP-ENV:Body/SOAP-ENV:Fault/faultstring", NAMESPACES)
-    
+
     fault_code = fault_code_el.text
     fault_string = fault_string_el.text
 
@@ -45,7 +45,7 @@ def _check_for_usgs_error(root):
 def _get_extended(scene, resp):
     """
     Parse metadata returned from the metadataUrl of a USGS scene.
-    
+
     :param scene:
         Dictionary representation of a USGS scene
     :param resp:
@@ -54,7 +54,7 @@ def _get_extended(scene, resp):
     root = ElementTree.fromstring(resp.text)
     items = root.findall("eemetadata:metadataFields/eemetadata:metadataField", NAMESPACES)
     scene['extended'] = { item.attrib.get('name').strip(): xsi.get(item[0]) for item in items }
-    
+
     return scene
 
 
@@ -62,11 +62,11 @@ def _async_requests(urls):
     """
     Sends multiple non-blocking requests. Returns
     a list of responses.
-    
+
     :param urls:
         List of urls
     """
-    
+
     session = FuturesSession(max_workers=30)
     futures = [ session.get(url) for url in urls ]
     return [ future.result() for future in futures ]
@@ -79,140 +79,140 @@ def _get_metadata_url(scene):
 def clear_bulk_download_order():
     raise NotImplementedError
 
-    
+
 def clear_order():
     raise NotImplementedError
-    
+
 
 def datasets(dataset, node, ll=None, ur=None, start_date=None, end_date=None):
-    
+
     api_key = _get_api_key()
-    
+
     xml = soap.datasets(dataset, node, ll=ll, ur=ur, start_date=start_date, end_date=end_date, api_key=api_key)
     r = requests.post(USGS_API, xml)
-    
+
     root = ElementTree.fromstring(r.text)
     _check_for_usgs_error(root)
-    
+
     items = root.findall("SOAP-ENV:Body/ns1:datasetsResponse/return/item", NAMESPACES)
-    
+
     data = map(lambda item: { el.tag: xsi.get(el) for el in item }, items)
-    
+
     return data
 
 
 def dataset_fields(dataset, node):
-    
+
     api_key = _get_api_key()
-    
+
     xml = soap.dataset_fields(dataset, node, api_key=api_key)
     r = requests.post(USGS_API, xml)
-    
+
     root = ElementTree.fromstring(r.text)
     _check_for_usgs_error(root)
-    
+
     items = root.findall("SOAP-ENV:Body/ns1:datasetFieldsResponse/return/item", NAMESPACES)
     data = map(lambda item: { el.tag: xsi.get(el) for el in item }, items)
-    
+
     return data
-    
+
 
 def download(dataset, node, entityids, product):
     """
     Though USGS supports multiple products in a single request, there's
     ambiguity in the returned list. This wrapper only allows a single
     product per request.
-    
+
     Additionally, the response has no indiction which URL is associated
     with which scene/entity id. The URL can be parsed, but the structure
     varies depending on the product.
     """
-    
+
     api_key = _get_api_key()
 
     xml = soap.download(dataset, node, entityids, [product], api_key=api_key)
     r = requests.post(USGS_API, xml)
-    
+
     root = ElementTree.fromstring(r.text)
     _check_for_usgs_error(root)
-    
+
     items = root.findall("SOAP-ENV:Body/ns1:downloadResponse/return/item", NAMESPACES)
-    
+
     data = map(lambda el: xsi.get(el), items)
-    
+
     return data
-    
+
 
 def download_options(dataset, node, entityids):
-    
+
     api_key = _get_api_key()
-    
+
     xml = soap.download_options(dataset, node, entityids, api_key=api_key)
     r = requests.post(USGS_API, xml)
-    
+
     root = ElementTree.fromstring(r.text)
     _check_for_usgs_error(root)
-    
+
     items = root.findall("SOAP-ENV:Body/ns1:downloadOptionsResponse/return/item/downloadOptions/item", NAMESPACES)
-    
+
     data = map(lambda item: { el.tag: xsi.get(el) for el in item }, items)
-    
+
     return data
 
 
 def get_bulk_download_products():
     raise NotImplementedError
-    
+
 
 def get_order_products():
     raise NotImplementedError
-    
+
 
 def hits():
     raise NotImplementedError
-    
+
 
 def item_basket():
     raise NotImplementedError
-    
-    
+
+
 def login(username, password):
     xml = soap.login(username, password)
     r = requests.post(USGS_API, xml)
-    
+
     if r.status_code is not 200:
         raise USGSError(r.text)
 
     root = ElementTree.fromstring(r.text)
     _check_for_usgs_error(root)
-    
+
     element = root.find("SOAP-ENV:Body/ns1:loginResponse/return", NAMESPACES)
-    
+
     api_key = element.text
-    
+
     with open(TMPFILE, "w") as f:
         f.write(api_key)
-    
+
     return api_key
-    
-    
+
+
 def logout():
-    
+
     api_key = _get_api_key()
-    
+
     xml = soap.logout(api_key=api_key)
     r = requests.post(USGS_API, xml)
-    
+
     if os.path.exists(TMPFILE):
         os.remove(TMPFILE)
-    
+
     return True
-    
+
 
 def metadata(dataset, node, sceneids, extended=False, api_key=None):
     """
     Request metadata for a given scene in a USGS dataset.
-    
+
     :param dataset:
     :param node:
     :param sceneids:
@@ -221,32 +221,32 @@ def metadata(dataset, node, sceneids, extended=False, api_key=None):
     :param api_key:
     """
     api_key = _get_api_key()
-    
+
     xml = soap.metadata(dataset, node, sceneids, api_key=api_key)
     r = requests.post(USGS_API, xml)
-    
+
     root = ElementTree.fromstring(r.text)
     _check_for_usgs_error(root)
-    
+
     items = root.findall("SOAP-ENV:Body/ns1:metadataResponse/return/item", NAMESPACES)
-    
+
     data = map(lambda item: { el.tag: xsi.get(el) for el in item }, items)
-    
+
     if extended:
         metadata_urls = map(_get_metadata_url, data)
         results = _async_requests(metadata_urls)
         data = map(lambda idx: _get_extended(data[idx], results[idx]), range(len(data)))
-    
+
     return data
-    
+
 
 def remove_bulk_download_scene():
     raise NotImplementedError
-    
+
 
 def remove_order_scene():
     raise NotImplementedError
-    
+
 
 def search(dataset, node, lat=None, lng=None, distance=100, ll=None, ur=None, start_date=None, end_date=None, where=None, max_results=50000, starting_number=1, sort_order="DESC", extended=False, api_key=None):
     """
@@ -313,27 +313,27 @@ def search(dataset, node, lat=None, lng=None, distance=100, ll=None, ur=None, st
     items = root.findall("SOAP-ENV:Body/ns1:searchResponse/return/results/item", NAMESPACES)
 
     data = map(lambda item: { el.tag: xsi.get(el) for el in item }, items)
-    
+
     if extended:
         metadata_urls = map(_get_metadata_url, data)
         results = _async_requests(metadata_urls)
         data = map(lambda idx: _get_extended(data[idx], results[idx]), range(len(data)))
-    
+
     return data
 
 
 def submit_bulk_order():
     raise NotImplementedError
-    
+
 
 def submit_order():
     raise NotImplementedError
-    
+
 
 def update_bulk_download_scene():
     raise NotImplementedError
-    
+
 
 def update_order_scene():
     raise NotImplementedError
-    
+
